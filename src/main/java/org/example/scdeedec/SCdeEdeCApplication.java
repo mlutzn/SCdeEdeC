0866043c473d1a9ff1090c2f013826cf1086fc0f
mlutzn <mlutzn@ucenfotec.ac.cr>
2026-07-14 21:11:20 -0600

    Base del proyecto

---
+++ src/main/java/org/example/scdeedec/SCdeEdeCApplication.java
@@ -0,0 +1,13 @@
+package org.example.scdeedec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SCdeEdeCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SCdeEdeCApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/scdeedec/SCdeEdeCApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.scdeedec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SCdeEdeCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
