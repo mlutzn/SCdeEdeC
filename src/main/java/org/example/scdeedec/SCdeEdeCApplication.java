2e174a98ac87423ad1cc66f4d792bacb1bd2a36b
mlutzn <mlutzn@ucenfotec.ac.cr>
2026-07-14 21:19:03 -0600

    Base del proyecto, estructura

--- src/main/java/org/example/scdeedec/SCdeEdeCApplication.java
+++ src/main/java/org/example/scdeedec/SCdeEdeCApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SCdeEdeCApplication {
+public class  SCdeEdeCApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SCdeEdeCApplication.class, args);
